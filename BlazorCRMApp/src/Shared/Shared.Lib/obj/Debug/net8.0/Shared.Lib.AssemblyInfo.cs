580c67dff7c5d4516ce32ce8e2b54d597c7457c4
Vinod Kumar <vinod.kumar1@dotsquares.com>
2024-10-23 14:26:22 +0530

    Develop commit

--- BlazorCRMApp/src/Shared/Shared.Lib/obj/Debug/net8.0/Shared.Lib.AssemblyInfo.cs
+++ BlazorCRMApp/src/Shared/Shared.Lib/obj/Debug/net8.0/Shared.Lib.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shared.Lib")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66af21c27cd9f3e0535151449b63fa08d13ed8e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shared.Lib")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shared.Lib")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
